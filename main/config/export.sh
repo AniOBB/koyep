f9879a4a45e427d0d907176b9ddc7fc76a875e0c
AniOBB <90925790+AniOBB@users.noreply.github.com>
2026-04-02 05:49:59 +0000

    Update export.sh

--- main/config/export.sh
+++ main/config/export.sh
@@ -3,9 +3,9 @@
 export PORT=${PORT:-80}
 export USERNAME=${USERNAME:-admin}
 export PASSWORD=${PASSWORD:-admin}
-export UUID=${UUID:-1eb6e917-774b-4a84-aff6-b058577c60a5}
-export PATH_VLESS=${PATH_VLESS:-/$UUID/vless}
-export PATH_VMESS=${PATH_VMESS:-/$UUID/vmess}
+export UUID=${UUID:-e719b70b-b59b-47f9-8462-7ceb44c730dd}
+export PATH_VLESS=${PATH_VLESS:-/$UUID/vls}
+export PATH_VMESS=${PATH_VMESS:-/$UUID/vms}
 export WARP_SERVER=${WARP_SERVER:-engage.cloudflareclient.com}
 export WARP_KEY=${WARP_KEY}
 export TUNNEL=${TUNNEL:-0}
